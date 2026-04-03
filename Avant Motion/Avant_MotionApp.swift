6defc700207d1ed05ae7b1270776d64e6f9d8cb1
pwtoothless <pwtoothless63@gmail.com>
2026-04-03 12:34:04 -0700

    Initial Commit

---
+++ Avant Motion/Avant_MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avant_MotionApp.swift
+//  Avant Motion
+//
+//  Created by Peyton Ward on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Avant_MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
